501f7697096395d7dacad34464bf63491497b2a2
Anan25 <anan25@example.com>
2025-12-31 10:17:03 +0700

    Thêm route /sepay/health

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('Hello Cloud Run!');
-});
 app.get('/sepay/health', (req, res) => {
   res.json({ ok: true, time: new Date().toISOString() });
+});
+app.get('/', (req, res) => {
+  res.send('Hello Cloud Run!');
 });
 
 app.listen(PORT, () => {
